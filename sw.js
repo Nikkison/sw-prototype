ed5f7e3b6e450101ea7878228cba2a2e5775376a
Unknown <huntingfield13@gmail.com>
2018-02-27 12:22:14 +0900

    jQueryもCDNから取得して、ローカルにキャッシュ

--- sw.js
+++ sw.js
@@ -1,12 +1,17 @@
 var CACHE_NAME = "my-site-cache-v1";
-var urlsToCache = ["/", "/main.css", "/main.js"];
+var urlsToCache = [
+    "/",
+    "/main.css",
+    "/main.js",
+    "//code.jquery.com/jquery-3.3.1.min.js"
+];
 
 self.addEventListener("install", function(event) {
     // インストール処理
     event.waitUntil(
         // キャッシュしただけではそのキャッシュが使われるわけではない
         caches.open(CACHE_NAME).then(function(cache) {
-            console.log("Opened cache");
+            console.log("Opened Cache:", urlsToCache);
             return cache.addAll(urlsToCache);
         })
     );
