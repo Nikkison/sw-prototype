d70684be2de9b523e79352f9002542c365f8ec03
Unknown <huntingfield13@gmail.com>
2018-02-27 20:13:06 +0900

    fetchイベント定義

--- sw.js
+++ sw.js
@@ -17,3 +17,44 @@ self.addEventListener("install", function(event) {
         })
     );
 });
+
+// リソース取得イベントが発火したときに走るコールバック
+self.addEventListener("fetch", function(event) {
+    console.log(event.request);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            // キャッシュがあったのでレスポンスを返す
+            if (response) {
+                console.log(response);
+                return response;
+            }
+
+            // 重要：リクエストを clone する。リクエストは Stream なので
+            // 一度しか処理できない。ここではキャッシュ用、fetch 用と2回
+            // 必要なので、リクエストは clone しないといけない
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(function(response) {
+                // レスポンスが正しいかをチェック
+                if (
+                    !response ||
+                    response.status !== 200 ||
+                    response.type !== "basic"
+                ) {
+                    return response;
+                }
+
+                // 重要：レスポンスを clone する。レスポンスは Stream で
+                // ブラウザ用とキャッシュ用の2回必要。なので clone して
+                // 2つの Stream があるようにする
+                var responseToCache = response.clone();
+
+                caches.open(CACHE_NAME).then(function(cache) {
+                    cache.put(event.request, responseToCache);
+                });
+
+                return response;
+            });
+        })
+    );
+});
